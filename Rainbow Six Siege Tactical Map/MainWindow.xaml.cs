deb847b77cecf52d4332934424c071ab6fb992e5
Lordi255 <51966991+Lordi255@users.noreply.github.com>
2019-06-19 01:52:59 +0200

    Update MainWindow.xaml.cs

--- Rainbow Six Siege Tactical Map/MainWindow.xaml.cs
+++ Rainbow Six Siege Tactical Map/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,19 +19,25 @@ namespace R6
     /// Logika interakcji dla klasy MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {/// <summary>
+     /// Okno startowe
+     /// </summary>
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Przechodzi do wyboru map
+        /// </summary>
         private void Map_Button_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             MapWindow mw = new MapWindow();
             mw.ShowDialog();
             this.Close();
         }
-
+        /// <summary>
+        /// Kliknięcie przycisku
+        /// </summary>
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
