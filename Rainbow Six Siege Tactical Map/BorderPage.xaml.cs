e2abf6433f4cd5adcc2ba8cd50c585275517f339
Lordi255 <51966991+Lordi255@users.noreply.github.com>
2019-06-19 01:38:27 +0200

    Add files via upload

---
+++ Rainbow Six Siege Tactical Map/BorderPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//Biblioteki potrzebne do Painta
+using System.Windows.Ink;
+using System.Windows.Navigation;
+using System.Reflection;
+
+namespace R6
+{
+    /// <summary>
+    /// Logika interakcji dla klasy BorderPage.xaml
+    /// </summary>
+    public partial class BorderPage : Page
+    {
+        public BorderPage()
+        {
+            InitializeComponent();
+            //Wypełnienie niepotrzebnych pól tablicy
+            Floor1[0] = true;
+            Floor2[0] = true;
+            //Metoda z tablicą obiektów
+            WallList();
+        }
+
+        //Liczba ścian na piętrze
+        const int wf1 = 2;
+        //Tablica zapisująca ściany
+        private bool[] Floor1 = new bool[wf1];
+        private bool[] Floor2 = new bool[35];
+        //Tablica obiektów
+        private Grid[] WallsFloor1 = new Grid[35];
+
+        //Metoda z tablicą obiektów
+        private void WallList()
+        {
+            WallsFloor1[1] = Wall1;
+        }
+
+        //Ładowanie 1 piętra
+        private void Floor1Load()
+        {
+            for (int i = 1; i < wf1; i++)
+            {
+                WallsFloor1[i].Visibility = (Floor1[1] == true) ? Visibility.Hidden : Visibility.Visible;
+            }
+        }
+
+        //Chowanie 1 piętra
+        private void Floor1Hidding()
+        {
+            for (int i = 1; i < wf1; i++)
+                WallsFloor1[i].Visibility = Visibility.Hidden;
+        }
+
+        //Metoda ukrywająca ścianę
+        private void Hidding(int i)
+        {
+            if (WallsFloor1[i].Opacity == 100)
+            {
+                WallsFloor1[i].Opacity = 0;
+                Floor1[i] = true;
+            }
+            else
+            {
+                WallsFloor1[i].Opacity = 100;
+                Floor1[i] = false;
+            }
+        }
+
+        private void Wall1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
